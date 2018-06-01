a9f96c46d5b284fd8422f352f5be4d9ac5446be7
TheSignLab Co <34943861+TheSignLab@users.noreply.github.com>
2018-06-01 14:43:37 -0500

    Auto Respuesta

--- src/backend/contact.php
+++ src/backend/contact.php
@@ -96,7 +96,7 @@ try {
     // Add a recipient
     $mail->addAddress('contact@autostudio-cr.com');
     $mail->addReplyTo('contact@autostudio-cr.com');  
-    $message = file_get_contents('templates/emailAdmissions.html'); 
+    $message = file_get_contents('templates/emailInfo.html'); 
     $message = str_replace('{{ThisName}}', $inName, $message);
     $message = str_replace('{{ThisPhone}}', $inPhone, $message);
     $message = str_replace('{{ThisEmail}}', $inEmail, $message);
@@ -126,6 +126,65 @@ try {
      echo '<br>';
     echo 'Message : '. $inMessage;
 }    
+
+
+
+
+
+try {
+    //Server settings
+
+    $mail->SMTPDebug = 0;                                 // Enable verbose debug output
+    $mail->isSMTP();                                      // Set mailer to use SMTP
+    $mail->Mailer = "smtp"; 
+    $mail->Host = 'a2plcpnl0095.prod.iad2.secureserver.net';  // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+    $mail->Username = 'contact@autostudio-cr.com';                 // SMTP username
+    $mail->Password = 'mail/wizard1.html';                           // SMTP password
+
+    $mail->Port =25;                                    // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom('contact@autostudio-cr.com', 'AutoStudio!');
+    // Add a recipient
+    $mail->addAddress($inEmail);
+    $mail->addReplyTo('contact@autostudio-cr.com');  
+    $message = file_get_contents('templates/emailClient.html'); 
+    $message = str_replace('{{ThisName}}', $inName, $message);
+    $message = str_replace('{{ThisPhone}}', $inPhone, $message);
+    $message = str_replace('{{ThisEmail}}', $inEmail, $message);
+
+
+    //Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'AutoStudio  | Gracias por confiar en nosotros '.$inName;
+    $mail->Body    = 'Gracias por confiar en nosotros '.$inName.' | Para mas información, ver en Autostudio-cr.com';
+    $mail->msgHTML($message, dirname(__FILE__));
+    $mail->AltBody = 'Gracias por confiar en nosotros '.$inName.' | Para mas información, ver en Autostudio-cr.com';
+
+    $mail->send();
+    
+    
+    echo "Pronto nos contactaremos contigo ".$inName;
+
+} catch (Exception $e) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+    echo '<br>';
+    echo 'Name : '. $inName;
+     echo '<br>';
+    echo 'Email : '. $inEmail;
+     echo '<br>';
+    echo 'Phone : '. $inPhone;
+     echo '<br>';
+    echo 'Message : '. $inMessage;
+}    
+
+
+
+
+
+
 
 
 
