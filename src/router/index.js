c6cbf08cec5790a14baf56b5536d3cc06348d6c3
TheSignLab Co <34943861+TheSignLab@users.noreply.github.com>
2018-06-19 15:51:27 -0500

    dfsd
    dfgd

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Home from '@/components/Home'
+import Admin from '@/components/Admin'
 
 Vue.use(Router)
 
@@ -8,16 +9,12 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'Home',
+      component: Home
     },{
-      path: '/services',
-      name: 'Services',
-      component: Services
-    },{
-      path: '/',
-      name: 'Hello',
-      component: Hello
-    },
+      path: '/admin',
+      name: 'Admin',
+      component: Info
+    }
   ]
 })
