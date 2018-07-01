446acf9d9a5ae3e933817d59328b5c9d14385858
TheSignLab Co <34943861+TheSignLab@users.noreply.github.com>
2018-07-01 15:36:01 -0500

    asd
    asda

--- src/views/home/home_desktop.vue
+++ src/views/home/home_desktop.vue
@@ -1,6 +1,60 @@
 <template>
   <div class="device desktop" id="desktop-home">
-    <h1> HOME  -  DESKTOP </h1>
+
+    <header class="header">
+      <img  class="header-logo" src="" alt="">
+      <ul class="header-menu">
+        <li></li>
+        <li></li>
+      </ul>
+    </header>
+
+    <section class="introduction">
+      <div class="wrapper">
+
+        <div class="container content">
+        <h1 class="title"></h1>
+        <h2 class="subtitle"></h2>
+        <button class="button">Solicitar Servicio</button>
+        <button class="button">¿Como funciona?</button>
+        </div>
+
+        <div class="container image">
+          <img src="" alt="AutoStudio">
+        </div>
+
+      </div>
+    </section>
+
+    <section class="services">
+       <div class="wrapper">
+       </div>
+    </section>
+
+    <section class="benefits">
+      <div class="wrapper"></div>
+    </section>
+
+    <section class="how-it-works">
+       <div class="wrapper"></div>
+    </section>
+
+    <section class="stories">
+      <div class="wrapper"></div>
+    </section>
+
+    <section class="real-time-map">
+      <div class="wrapper"></div>
+    </section>
+
+    <section class="contact-us">
+      <div class="wrapper"></div>
+    </section>
+
+    <footer class="footer">
+      <div class="wrapper"></div>
+    </footer>
+
   </div>
 </template>
 
@@ -10,6 +64,13 @@
   }
 </script>
 
-<style>
-
+<style scoped>
+.wrapper{
+  display: block;
+  width: auto;
+  max-width: 1100px;
+  margin: 0 auto;
+  height: 100vh;
+  min-height: 100vh;
+}
 </style>
